ef2177d545bb6c62d3683edc72317403efa6bf1b
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2016-08-28 14:12:11 -0300

    adding comments to facilitate understanding of the code

--- PIC16F628A.X/sleep_check.asm
+++ PIC16F628A.X/sleep_check.asm
@@ -1,6 +1,6 @@
 ; PIC16F628A Configuration Bit Settings
 ; ASM source line config statements
-#include "p16F628A.inc"
+#include "p16f628a.inc"
 
 ; CONFIG
 ; __config 0xFF19
@@ -14,48 +14,58 @@ can_sleep   EQU	0x00
 	GOTO	setup
 	
 	ORG	0x0004
-	BTFSC	INTCON , T0IF
-	CALL	i_tar
-	BTFSC	INTCON, INTF
+	BTFSC	INTCON , T0IF		;check timer interrupt
+	CALL	timer
+	BTFSC	INTCON, INTF		;check button interrupt
 	CALL	button
 	RETFIE
 	
-i_tar:		
-	BCF	INTCON, T0IF	    ;clear timer interrupt flag
-	BSF	INTCON, INTE	    ;enable rb0 interrupt
+timer:		
+	BCF	INTCON, T0IF		;clear timer interrupt flag
+	BSF	INTCON, INTE		;enable rb0 interrupt
 	BSF	control_reg, can_sleep  ;enable sleep
 	RETURN
 	
 button:
-	BCF	INTCON, INTF	    ;clear rb0 interrupt flag
-	INCF	PORTA		    ;inc porta
-	BCF	INTCON, INTE	    ;disable rb0 interrupt
-	MOVWF	iCount		    ;set timer initial value
+	BCF	INTCON, INTF		;clear rb0 interrupt flag
+	INCF	PORTA			;inc porta
+	BCF	INTCON, INTE		;disable rb0 interrupt
+	MOVWF	iCount			;set timer initial value
 	MOVWF	TMR0
 	BCF	control_reg, can_sleep  ;disable sleep
 	RETURN
 	
-setup:	
-	CLRF PORTA
+setup:				
 	BANKSEL	CMCON
-	MOVLW 0x07
+	MOVLW 0x07			;disable comparators
 	MOVWF CMCON
     
 	BANKSEL	TRISA
-	CLRF	TRISA	
-	BSF	TRISB, RB0
+	CLRF	TRISA			;setting porta as output
+	BSF	TRISB, RB0		;set rb0 as input
 	
-	BANKSEL	OPTION_REG
-	MOVLW	b'11010011'
-	MOVWF	OPTION_REG
-	BCF	PCON, OSCF
-	MOVLW	b'10110000'
-	MOVWF	INTCON
-	
+	BANKSEL	OPTION_REG		
+	MOVLW	b'11010011'		;7  |1   - portb pull-up disable
+	MOVWF	OPTION_REG		;6  |1   - interrup in rising edge
+					;5  |0   - internal clock
+					;4  |1   - dont care
+					;3  |0   - prescalar in timer0
+					;2~0|011 - prescalar 1:16
+					
+	BCF	PCON, OSCF		;setting internal clock as 48kHz
+	MOVLW	b'10110000'		;7|1 - enable global interrupts
+	MOVWF	INTCON			;6|0 - disable peripheral interrupts
+					;5|1 - enable TMR0 interrupt
+					;4|1 - enable RB0 interrupt
+					;3|0 - dont care
+					;2|0 - dont care
+					;1|0 - dont care
+					;0|0 - dont care
 	BANKSEL	PORTA
-	MOVLW	iCount
+	CLRF	PORTA			;cleaning up porta
+	MOVLW	iCount			;setting inital value of timer as iCount
 	MOVWF	TMR0
-	BSF	control_reg, can_sleep
+	BSF	control_reg, can_sleep	;enable sleep
 	
 main:	BTFSC	control_reg, can_sleep
 	SLEEP
