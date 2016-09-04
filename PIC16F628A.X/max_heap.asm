51c3002dc1bf72b40a65ce607bdace413911f576
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2016-09-04 14:37:46 -0300

    fixing bugs

--- PIC16F628A.X/max_heap.asm
+++ PIC16F628A.X/max_heap.asm
@@ -12,16 +12,17 @@ count	    EQU h'21'
 array_adr   EQU h'22'
 	
 ;BANK 1
+exit	    EQU h'20'
 i	    EQU h'21'
-exit	    EQU h'22'
-j	    EQU h'23'
-left	    EQU h'24'
-right	    EQU h'25'
-largest	    EQU h'26'
-index	    EQU h'27'
-first	    EQU	h'28'
-second	    EQU	h'29'
-aux	    EQU	h'2A'
+j	    EQU h'22'
+left	    EQU h'23'
+right	    EQU h'24'
+largest	    EQU h'25'
+index	    EQU h'26'
+first	    EQU	h'27'
+second	    EQU	h'28'
+aux	    EQU	h'29'
+index2	    EQU	h'2A'
 	
 	ORG 0x0000  
 	GOTO setup
@@ -52,7 +53,7 @@ read_loop:
 	
 	MOVF	count, W	;Read one byte of the EEPRON
 	CALL	EE_byte_read
-	BANKSEL	INDF
+	BANKSEL	PORTA
 	MOVWF	INDF
 	
 	INCF	FSR		;set next address of the array
@@ -64,9 +65,44 @@ read_loop:
 	RETURN
 	
 	
-get_element:
-	MOVF	array_adr, W ;set to initial position in array
+swap_elements:
+	CALL	get_element	;get first element
+	MOVWF	first
+	MOVF	index, W	;save index1 in aux
+	MOVWF	aux		
+	MOVF	index2, W	;get second element
+	MOVWF	index
+	CALL	get_element	
+	MOVWF	second
+	MOVF	aux, W		;restore index1
+	MOVWF	index
+	
+	BANKSEL	PORTA	    ;put second element in index1
+	MOVLW	array_adr
 	MOVWF	FSR
+	BANKSEL	TRISA
+	MOVF	index, W
+	ADDWF	FSR
+	MOVF	second, W
+	MOVWF	INDF
+	
+	BANKSEL	PORTA	    ;put first element in index2
+	MOVLW	array_adr
+	MOVWF	FSR
+	BANKSEL	TRISA
+	MOVF	index2, W
+	ADDWF	FSR
+	MOVF	first, W
+	MOVWF	INDF
+	
+	RETURN
+    
+	
+get_element:
+	BANKSEL	PORTA
+	MOVLW	array_adr ;set to initial position in array
+	MOVWF	FSR
+	BANKSEL	TRISA
 	MOVF	index, W     ;offset to element index
 	ADDWF	FSR
 	MOVF	INDF, W	     ;put the element value in W_REG
@@ -82,10 +118,12 @@ max_heapfy_loop:
 ;-------------------SETUP----------------------    
 	MOVF	j, W	;left = 2*j
 	MOVWF	left
+	BCF	STATUS, C
 	RLF	left	
 	
 	MOVF	j, W	;right = 2*j+1
 	MOVWF	right
+	BCF	STATUS, C
 	RLF	right
 	INCF	right	
     
@@ -94,15 +132,21 @@ max_heapfy_loop:
 ;-------------------LEFT----------------------
 	;REG >= W -> C = 1
 	MOVF	left, W	    ;check if left <= EE_size
+	BANKSEL	PORTA
 	SUBWF	EE_size, W
+	BANKSEL	TRISA
 	BTFSS	STATUS, C
 	GOTO	skip_left
 	
 	MOVF	left, W	    ;get A[left]
+	MOVWF	index
+	DECF	index
 	CALL	get_element
 	MOVWF	first
 	
 	MOVF	largest, W  ;get A[largest]
+	MOVWF	index
+	DECF	index
 	CALL	get_element
 	MOVWF	second
 	
@@ -112,17 +156,27 @@ max_heapfy_loop:
 	GOTO	skip_left
 	
 	MOVF	left, W	    ;largest = left
-	MOVWF	largets
+	MOVWF	largest
 skip_left:
 ;-------------------RIGHT----------------------
 	MOVF	right, W    ;check if right <= EE_size
+	BANKSEL	PORTA
 	SUBWF	EE_size, W
+	BANKSEL	TRISA
 	BTFSS	STATUS, C
 	GOTO	skip_right
 	
 	MOVF	right, W    ;get A[right]
+	MOVWF	index
+	DECF	index
 	CALL	get_element
 	MOVWF	first
+	
+	MOVF	largest, W  ;get A[largest]
+	MOVWF	index
+	DECF	index
+	CALL	get_element
+	MOVWF	second
 	
 	MOVF	second, W   ;check if A[right] >= A[largest]
 	SUBWF	first, W
@@ -130,29 +184,22 @@ skip_left:
 	GOTO	skip_right
 	
 	MOVF	right, W    ;largest = left
-	MOVWF	largets
+	MOVWF	largest
 skip_right:
 ;---------------------SWAP----------------------
-	MOVF	j
+	MOVF	j, W
 	SUBWF	largest, W
 	BTFSS	STATUS, Z
 	GOTO	diff
 	GOTO	equal
 diff:
-	MOVF	j, W    ;get A[j]
-	CALL	get_element
-	MOVWF	first
-	
-	MOVF	largest, W    ;get A[largest]
-	CALL	get_element
-	MOVWF	second
-	
-	MOVF	first, W    ;swap A[j] with A[largest]
-	MOVWF	aux
-	MOVF	second, W
-	MOVWF	first
-	MOVF	aux, W
-	MOVWF	second
+	MOVF	j, W    ;swap A[j] with A[largest]
+	MOVWF	index
+	DECF	index
+	MOVF	largest, W
+	MOVWF	index2
+	DECF	index2
+	CALL	swap_elements
 	
 	MOVF	largest, W  ;j = largest
 	MOVWF	j
@@ -167,11 +214,15 @@ final:
 	
 	
 build_max_heap:
-	RRF	EE_size, W
+	BANKSEL	PORTA
+	BCF	STATUS, C
+	RRF	EE_size, 0
+	BANKSEL	TRISA
 	MOVWF	i	    ;i <- EE_size/2
 build_max_heap_loop:
 	CALL	max_heapfy
-    
+	
+	BANKSEL	TRISA
     	DECFSZ	i	    ;for EE_size/2 to 1
 	GOTO	build_max_heap_loop
 	RETURN
@@ -179,6 +230,7 @@ build_max_heap_loop:
 	
 setup:
 	CALL	EE_data_read
+	CALL	build_max_heap
     
 main:	
 	GOTO main
